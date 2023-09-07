2c84003c1ca160a9a37d93826dc08bbf2419c298
Hendra Lin <info@bengkel-oprek.web.id>
2023-09-07 16:34:13 +0700

    feat: add authorization

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,35 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
+/**
+ * Healthcheck
+ *
+ * Check that the service is up. If everything is okay, you'll get a 200 OK response.
+ *
+ * Otherwise, the request will fail with a 400 error, and a response listing the failed services.
+ *
+ * @response 400 scenario="Service is unhealthy" {"status": "down", "services": {"database": "up", "redis": "down"}}
+ * @responseField status The status of this API (`up` or `down`).
+ * @responseField services Map of each downstream service and their status (`up` or `down`).
+ */
+
+Route::get('healthcheck', function () {
+    return [
+        'status' => 'up',
+        'services' => [
+            'database' => 'up',
+            'redis' => 'up',
+        ],
+    ];
+})->name('healthcheck');
+
+/**
+ * User Profile
+ *
+ * Get user profile
+ *
+ * @authenticated
+ */
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -24,6 +53,14 @@ Route::prefix('v1/auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
+
+/**
+ * Fallback
+ *
+ * This is a fallback route
+ *
+ * @response 404 scenario="Page Not Found" {"message": "Page Not Found"}
+ */
 
 Route::fallback(function () {
     return response()->json([
